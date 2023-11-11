9f0f0744e46ae6081c58e5453ef92627445e5911
dwi <wdwihartarto@gmail.com>
2023-11-11 22:57:11 +0700

    finish implement rxjava

--- app/src/main/java/com/dicoding/tourismapp/domain/usecase/TourismInteractor.kt
+++ app/src/main/java/com/dicoding/tourismapp/domain/usecase/TourismInteractor.kt
@@ -4,12 +4,13 @@ import androidx.lifecycle.LiveData
 import com.dicoding.tourismapp.core.data.Resource
 import com.dicoding.tourismapp.domain.model.Tourism
 import com.dicoding.tourismapp.domain.repository.ITourismRepository
+import io.reactivex.Flowable
 
 class TourismInteractor(private val tourismRepository: ITourismRepository) : TourismUseCase {
-    override fun getAllTourism(): LiveData<Resource<List<Tourism>>> =
+    override fun getAllTourism(): Flowable<Resource<List<Tourism>>> =
         tourismRepository.getAllTourism()
 
-    override fun getFavoriteTourism(): LiveData<List<Tourism>> =
+    override fun getFavoriteTourism(): Flowable<List<Tourism>> =
         tourismRepository.getFavoriteTourism()
 
     override fun setFavoriteTourism(tourism: Tourism, state: Boolean) =
